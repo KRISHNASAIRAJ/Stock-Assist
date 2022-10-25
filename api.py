0924efd27644ffae5eadfb7852f972b944fc8991
KRISHNA SAI RAJ PONNEBOINA <90061814+KRISHNASAIRAJ@users.noreply.github.com>
2022-10-25 12:12:14 +0530

    Update api.py
    Updated RSI Indicator

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd#Analysis of data
 import yfinance as yf
 st.set_page_config(page_title='Stock Assist',page_icon='📈')
 st.title('Stock Assist')
@@ -9,5 +9,19 @@ tickers=[i.replace('.','-') for i in tickers]
 tickers.pop(474)
 tickers.pop(489)
 dropdown=st.multiselect('Pick your asset',tickers)
-start = st.date_input('Start', value = pd.to_datetime('2022-10-01'))
+startd = st.date_input('Start', value = pd.to_datetime('2022-10-01'))
 end = st.date_input('End', value = pd.to_datetime('today'))
+def RSI(asset):
+    dataframe = yf.download(asset,start=startd)
+    #dataframe['MA200']=dataframe['Adj Close'].rolling(window=200).mean()
+    dataframe['Price Change']=dataframe['Adj Close'].pct_change()
+    dataframe['Upmove']=dataframe['Price Change'].apply(lambda x: x if x>0 else 0)
+    dataframe['Downmove']=dataframe['Price Change'].apply(lambda x: abs(x) if x<0 else 0)
+    dataframe['AvgUp']=dataframe['Upmove'].ewm(span=19).mean()
+    dataframe['AvgDown']=dataframe['Downmove'].ewm(span=19).mean()
+    dataframe=dataframe.dropna()
+    dataframe['RS']=dataframe['AvgUp']/dataframe['AvgDown']
+    dataframe['RSI']=dataframe['RS'].apply(lambda x:100-(100/(1+x)))
+    #dataframe.loc[dataframe['']&(dataframe['RSI']<30),'Buy']='Yes'
+    return dataframe
+st.table(RSI(dropdown))
